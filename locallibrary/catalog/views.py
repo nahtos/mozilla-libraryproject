5ea74a168d59b1e1ee208563dcafe642a3f3025f
Michael <michael@eurt.com>
2021-11-17 22:24:42 +0000

    Added Author List and Author Detail views

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -35,4 +35,12 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
 	model = Book
 
+class AuthorListView(generic.ListView):
+	model = Author
+
+class AuthorDetailView(generic.DetailView):
+	model = Author
+	paginate_by = 2
+
+
 
